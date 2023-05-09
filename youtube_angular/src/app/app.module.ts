e4bd8b7d8829501975aae20c227165e0fdb93889
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-09 18:12:48 +0300

    FormsModule

--- youtube_angular/src/app/app.module.ts
+++ youtube_angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Layout/header/header.component';
 import { MainComponent } from './Layout/main/main.component';
@@ -13,12 +13,10 @@ import { FooterComponent } from './Layout/footer/footer.component';
     HeaderComponent,
     MainComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
