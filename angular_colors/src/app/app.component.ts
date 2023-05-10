593bb59d4dae36e57006ab5bca5157bffcfe57a3
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-10 17:41:12 +0300

    function

--- angular_colors/src/app/app.component.ts
+++ angular_colors/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular_colors';
+  myName = 'Shnitzel';
+  myType = 'Gaver';
+
+  showNameAndType() {
+    return `My name is ${this.myName} and my type is ${this.myType}`;
+  }
 }
