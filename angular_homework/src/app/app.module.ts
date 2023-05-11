b3e4de8aacc5f1e5acaeadc00216663309b7847c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-11 14:14:51 +0300

    BINDING CUSTOM PROP

--- angular_homework/src/app/app.module.ts
+++ angular_homework/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GreenDirective } from './dir/green.directive';
+import { ListComponent } from './comps/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, GreenDirective],
+  declarations: [AppComponent, GreenDirective, ListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
