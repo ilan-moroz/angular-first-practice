0a85d90e4f50fed43ea5dcc3c3562eadb51935a7
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-12 12:52:34 +0300

    hostBinding

--- angular_homework2/src/app/dir/better-green.directive.ts
+++ angular_homework2/src/app/dir/better-green.directive.ts
@@ -1,22 +1,49 @@
-import { Directive, OnInit, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  ElementRef,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
+// hostListener
+// @Directive({
+//   selector: '[appBetterGreen]',
+// })
+// export class BetterGreenDirective implements OnInit {
+//   constructor(private elementRef: ElementRef) {}
+
+//   ngOnInit() {
+//פקודה של דיבור עם האלמנט שעליו יהיה את הדרקטייב שיהפוך את הרקע שלו לירוק
+//     this.elementRef.nativeElement.style.background = 'green';
+//     this.elementRef.nativeElement.style.color = 'white';
+//   }
+
+//   @HostListener('mouseenter') mouseover() {
+//     this.elementRef.nativeElement.style.color = 'red';
+//   }
+
+//   @HostListener('mouseleave') mouseleave() {
+//     this.elementRef.nativeElement.style.color = 'white';
+//   }
+// }
+
+// hostBinding
 @Directive({
   selector: '[appBetterGreen]',
 })
 export class BetterGreenDirective implements OnInit {
+  @HostBinding('style.color') clr = 'white';
+  @HostBinding('style.background') bg = 'greenyellow';
   constructor(private elementRef: ElementRef) {}
 
-  ngOnInit() {
-    //פקודה של דיבור עם האלמנט שעליו יהיה את הדרקטייב שיהפוך את הרקע שלו לירוק
-    this.elementRef.nativeElement.style.background = 'green';
-    this.elementRef.nativeElement.style.color = 'white';
-  }
+  ngOnInit() {}
 
   @HostListener('mouseenter') mouseover() {
-    this.elementRef.nativeElement.style.color = 'red';
+    this.clr = 'brown';
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.elementRef.nativeElement.style.color = 'white';
+    this.clr = 'white';
   }
 }
