4c5d03ae2f049fcaf0bd5d1b52be041900517789
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-14 13:44:08 +0300

    404

--- angular_with_routing/src/app/app-routing.module.ts
+++ angular_with_routing/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CryptoListComponent } from './comps/crypto-list/crypto-list.component';
 import { ItemsListComponent } from './comps/items-list/items-list.component';
 import { FoodsComponent } from './comps/foods/foods.component';
+import { PageError404Component } from './comps/page-error404/page-error404.component';
 
 const routes: Routes = [
   { path: '', component: CryptoListComponent },
   { path: 'list', component: ItemsListComponent },
   { path: 'foods', component: FoodsComponent },
   { path: 'foods/:id', component: FoodsComponent },
+  { path: '**', component: PageError404Component },
 ];
 
 @NgModule({
