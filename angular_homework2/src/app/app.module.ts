6d97fc56230a7e4c3c1a3ec2b7fae8ed2139f72b
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-14 12:18:10 +0300

    OBSERVABLE

--- angular_homework2/src/app/app.module.ts
+++ angular_homework2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BetterGreenDirective } from './dir/better-green.directive';
 import { InputUserComponent } from './comps/input-user/input-user.component';
 import { ItemsListComponent } from './comps/items-list/items-list.component';
+import { CryptoListComponent } from './comps/crypto-list/crypto-list.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { ItemsListComponent } from './comps/items-list/items-list.component';
     BetterGreenDirective,
     InputUserComponent,
     ItemsListComponent,
+    CryptoListComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
--- angular_homework2/src/app/services/crypto.service.ts
+++ angular_homework2/src/app/services/crypto.service.ts
@@ -6,5 +6,19 @@ import { Observable } from 'rxjs';
 })
 export class CryptoService {
   data = '';
+  getTestData() {
+    this.data = new Observable((obs) => {
+      setTimeout(() => {
+        obs.next('bit 1000');
+      }, 1000);
+      setTimeout(() => {
+        obs.next('bit value 1002');
+      }, 2000);
+      setTimeout(() => {
+        obs.next('bit value 3000');
+      }, 3000);
+    });
+    return this.data;
+  }
   constructor() {}
 }
