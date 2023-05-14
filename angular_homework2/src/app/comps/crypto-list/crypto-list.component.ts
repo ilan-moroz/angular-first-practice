ed67f8acb06e291d11d600c025f3e2ab941b8e5c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-14 12:50:09 +0300

    api

--- angular_homework2/src/app/comps/crypto-list/crypto-list.component.ts
+++ angular_homework2/src/app/comps/crypto-list/crypto-list.component.ts
@@ -7,11 +7,17 @@ import { CryptoService } from '../../services/crypto.service';
   styleUrls: ['./crypto-list.component.css'],
 })
 export class CryptoListComponent implements OnInit {
+  coins_ar: any = [];
   constructor(private _cryptoServ: CryptoService) {}
 
   ngOnInit() {
     this._cryptoServ.getTestData().subscribe((data: any) => {
       console.log(data);
+    });
+    this._cryptoServ.makeApiRequest().subscribe((data) => {
+      console.log(data);
+      this.coins_ar.push(...Object.values(data));
+      console.log(this.coins_ar);
     });
   }
 }
--- angular_homework2/src/app/services/crypto.service.ts
+++ angular_homework2/src/app/services/crypto.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CryptoService {
   data: any;
+  bitUrl =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
   getTestData() {
     this.data = new Observable((obs) => {
       setTimeout(() => {
@@ -20,5 +23,8 @@ export class CryptoService {
     });
     return this.data;
   }
-  constructor() {}
+  makeApiRequest() {
+    return this.http.get(this.bitUrl);
+  }
+  constructor(private http: HttpClient) {}
 }
