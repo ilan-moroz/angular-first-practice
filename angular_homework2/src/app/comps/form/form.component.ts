3edecdef59be4f0c7e98f0358f31fca0c84eb5e0
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-17 13:03:40 +0300

    //

--- angular_homework2/src/app/comps/form/form.component.ts
+++ angular_homework2/src/app/comps/form/form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormService } from 'src/app/services/form.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  mission = '';
+  start_time = '';
+  finish_time = '';
+  price = '';
+  constructor(private formService: FormService) {}
 
+  ngOnInit() {}
 }
